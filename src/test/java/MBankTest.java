3b31eb93614ec66e05b780765d03f6a85ae953ff
Ivan Dmitriev <dmitrievstekloy@gmail.com>
2020-07-06 17:13:15 +0300

    MBankTest added

---
+++ src/test/java/MBankTest.java
@@ -0,0 +1,31 @@
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+public class MBankTest {
+    @Test
+    void shouldReturnDemoAccounts () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("id:3")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("id:3"));
+    }
+
+    @Test
+    void shouldReturnDemoAccounts2 () {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("currency:RUB")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("currency:RUB"));
+    }
+}
